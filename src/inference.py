c33a5131008bb37226ad97befee0f1b3d69f8b9b
Mohammad Saleh Refahi <mohammadsaleh.refahi@gmail.com>
2024-11-08 11:19:06 -0500

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -230,8 +230,9 @@ def main(db_path, scorpio_model, output, test_fasta, max_len, batch_size, test_e
     
     if torch.cuda.device_count() == args.get("num_device"):
         index = faiss.index_cpu_to_all_gpus(index)
-    elif get_available_gpus(args.get("required_memory_gb"))[0]==1:
-        faiss.index_cpu_to_gpu(faiss.StandardGpuResources(), get_available_gpus(args.required_memory_gb)[0], index) 
+    elif len(get_available_gpus(args.get("required_memory_gb"))[:1])==1:
+        print("indexed GPU")
+        faiss.index_cpu_to_gpu(faiss.StandardGpuResources(), get_available_gpus(args.get("required_memory_gb"))[0], index) 
     else:
         pass
         
